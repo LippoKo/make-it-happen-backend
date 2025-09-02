de02a0a0220c49b7ed8cd444051c8b445ca6445a
Luiz Lima <97291114+LippoKo@users.noreply.github.com>
2025-09-02 11:26:53 +0100

    add CI workflow and update linting, service, and tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,13 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-describe('AppController', () => {
-  let appController: AppController;
+describe('AppController (e2e)', () => {
+  let service: AppService;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
-
-    appController = app.get<AppController>(AppController);
+  beforeEach(() => {
+    service = new AppService();
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
+  it('Should return hello object', () => {
+    expect(service.getHello()).toEqual('M-iT-H!');
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello() {
-    return { title: 'M-iT-H!' };
+  getHello(): string {
+    return 'M-iT-H!';
   }
 }
